9958521dd13480044d7c8016ccd92edcd306c66f
Nuno Nogueira <nunofmn@gmail.com>
2014-12-04 11:27:15 +0000

    Added Loadtesting script. Added some report data.

---
+++ loadtest.sh
@@ -0,0 +1,7 @@
+siege -c100 -t5M -d10 http://birdwatch-lb-525863057.us-west-2.elb.amazonaws.com/test
+sleep 30
+siege -c200 -t5M -d10 http://birdwatch-lb-525863057.us-west-2.elb.amazonaws.com/test
+sleep 30
+siege -c300 -t5M -d10 http://birdwatch-lb-525863057.us-west-2.elb.amazonaws.com/test
+sleep 30
+siege -c500 -t5M -d10 http://birdwatch-lb-525863057.us-west-2.elb.amazonaws.com/test
